233c488db4ef182ffb2074798d38c3ea6ec2aff9
Carlos Barria <cbarria@yahoo.com>
2024-03-26 19:31:14 -0300

    (cnpg-backup) fix missing space in script

--- image/cnpg-backup.sh
+++ image/cnpg-backup.sh
@@ -19,4 +19,4 @@ current_time=$(date "+%H-%M-%S-%d-%m-%Y")
 # Thinking of adding -c to pg_dumpall to wipe before recreate
 pg_dumpall -w -U postgres -h ${HOST} -f /tmp/cnpg-backup.sql
 tar -zcvf /tmp/cnpg-backup.tar.gz /tmp/cnpg-backup.sql
-aws s3 cp /tmp/cnpg-backup.tar.gz ${AWS_ACCESS_BUCKET}cnpg-backup-$current_time.tar.gz
+aws s3 cp /tmp/cnpg-backup.tar.gz ${AWS_ACCESS_BUCKET}/cnpg-backup-$current_time.tar.gz
